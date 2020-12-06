1ae6736223a1f8d6fec45a511c9aa6e2a82953bc
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-06 11:47:13 +0100

    add postgresql config

--- project/settings.py
+++ project/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbjedo',
+        'USER': 'postgres',
+        'PASSWORD': 'PASSWD',
+        'HOST'  : 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
