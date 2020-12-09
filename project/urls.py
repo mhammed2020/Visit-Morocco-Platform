63e49d2021f57b053600c2b81867ffcb85132cc1
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-09 19:39:26 +0100

    create login form with Class Based View method

--- project/urls.py
+++ project/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from  django.conf.urls.static import static
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
+
+        path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
