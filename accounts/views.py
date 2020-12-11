fd006025503698220d9ea8d267342ba075f40159
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-11 21:00:02 +0100

    add DestinationCreateView for accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 
 from django.views.generic import (
     ListView,
+    CreateView,
   
 )
 from . models import Destination
@@ -22,3 +23,11 @@ class DestinationListView(ListView):
     template_name = 'accounts/home.html'  
     context_object_name = 'destinations'
     paginate_by = 2
+
+class DestinationCreateView(CreateView):
+    model = Destination
+    fields = ['name', 'img','desc','price']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
