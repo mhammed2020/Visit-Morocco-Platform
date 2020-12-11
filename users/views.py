9c04c1102769070de8a82737195d2e85c19ac031
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-11 17:23:53 +0100

    change redirec registration to home page

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
+            return redirect('home')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
