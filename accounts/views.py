c9b3ece04ab13f66dd091ba15bb9d9df1d10a284
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-11 20:27:43 +0100

    add ListView class  to accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render,get_object_or_404
+
+from django.views.generic import (
+    ListView,
+  
+)
 from . models import Destination
 # Create your views here.
 def home(request) :
@@ -11,3 +16,9 @@ def home(request) :
     }
 
     return render(request,'accounts/home.html', context)
+
+class DestinationListView(ListView):
+    model = Destination
+    template_name = 'accounts/home.html'  
+    context_object_name = 'destinations'
+    paginate_by = 2
