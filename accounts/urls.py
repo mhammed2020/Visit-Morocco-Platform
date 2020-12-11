ccdc951738649b696df69bb3395b6c98ed346bed
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-11 21:21:26 +0100

    add post_detail function based view to urlpattern

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # path('', views.home, name='home')
     path('', DestinationListView.as_view(), name='home'),
     path('post/new/', DestinationCreateView.as_view(), name='post-create'),
+    path('detail/<int:post_id>/', views.post_detail, name='post-detail'),
 
 
 ]
