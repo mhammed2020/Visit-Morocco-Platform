6b45eb18b7ed1c2011d3bc7c22b3b834a7bdb534
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-12 19:05:43 +0100

    using function based views  in accounts views

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,8 +10,8 @@ from .views import (
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home')
-    path('', DestinationListView.as_view(), name='home'),
+    path('', views.home, name='home'),
+    # path('', DestinationListView.as_view(), name='home'),
     path('post/new/', DestinationCreateView.as_view(), name='post-create'),
     path('detail/<int:post_id>/', views.post_detail, name='post-detail'),
     path('post/<int:pk>/update/', DestinationUpdateView.as_view(), name='post-update'),
