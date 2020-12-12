29a659eb237d74577906f37b6116062f304be1e0
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-12 12:21:13 +0100

    add django backend config for sending e mail

--- project/settings.py
+++ project/settings.py
@@ -141,3 +141,14 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'google.0@gmail.com'
+EMAIL_HOST_PASSWORD = 'hapojkthfghhdjjjqrt'
