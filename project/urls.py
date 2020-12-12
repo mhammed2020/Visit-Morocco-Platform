bc4aedb76121d5a934ae2a8eb620a75949a594db
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-12 12:38:08 +0100

    add PasswordResetConfirmView

--- project/urls.py
+++ project/urls.py
@@ -42,5 +42,13 @@ path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users
  path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
          name='password_reset_done'),
 
+
+
+path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
