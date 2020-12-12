31516c4ca1fd335cb569a560881c3ff7eb9993f5
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-12 11:49:11 +0100

    add DeleteView

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.views.generic import (
     ListView,
     CreateView,
     UpdateView,
+    DeleteView
   
 )
 from . models import Destination
@@ -53,6 +54,17 @@ class DestinationUpdateView(UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(DeleteView):
+    model = Destination
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
