d61798d3904a1de82583de59a9110a3b9f3ff717
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-13 20:21:22 +0100

    create Info model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Info(models.Model):
+    name = models.CharField(max_length=50)
+    subject = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    message = models.TextField()
+
+    class Meta:
+        verbose_name = ("Info")
+        verbose_name_plural = ("Infos")
+
+    def __str__(self):
+        return self.email
