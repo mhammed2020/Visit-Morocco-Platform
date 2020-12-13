184ce521f15226093e87adb5116a742575fa662b
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-13 14:16:31 +0100

    add django mixins for LoginRequiredMixin and UserPassesTestMixin

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.views.generic import (
     ListView,
