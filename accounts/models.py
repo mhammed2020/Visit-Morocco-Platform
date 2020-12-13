7f2b182571ced8fa1d37d6ae1fc74e88aff60b79
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-13 12:57:46 +0100

    add date_posted field to Destination class

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Destination(models.Model) :
     name = models.CharField(max_length=100, blank=True, null=True)
@@ -9,6 +10,8 @@ class Destination(models.Model) :
     price = models.IntegerField()
     offer = models.BooleanField(default=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+
 
 
     def __str__(self):
