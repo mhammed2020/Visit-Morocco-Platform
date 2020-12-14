82de98cc79bc36d130519c7e4e3d82803f24c205
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-14 10:43:05 +0100

    add Django Extensions to the INSTALLED_APPS setting

--- project/settings.py
+++ project/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'django_extensions',
+    'django_extensions', #  for ssl
    
    
 
