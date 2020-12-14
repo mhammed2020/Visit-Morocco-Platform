8c9d92041acc2099c7645c65011ae74a11362dba
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-14 15:19:04 +0100

    add slugify function to generate automatically slug field

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='images_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -11,3 +12,8 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            super().save(*args, **kwargs)
