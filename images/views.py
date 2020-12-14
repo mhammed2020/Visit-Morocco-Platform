4cbb4aa9dfc2d5c64670a32e2ee2e2fff2910a24
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-14 17:02:23 +0100

    build image_create method as first view for images app

--- images/views.py
+++ images/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ImageCreateForm
 
-# Create your views here.
+def image_create(request):
+    if request.method == 'POST': # form saved
+    
+        form = ImageCreateForm(data=request.POST,files=request.FILES)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            return redirect(new_item.get_absolute_url())
+
+    else:
+            
+        form = ImageCreateForm(data=request.GET) # show Image form
+    return render(request, 'images/image/create.html', {'section': 'images','form': form})
