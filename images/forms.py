f33b477f41830aa488c7622cb2d22942472e82ae
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-14 16:11:16 +0100

    overriding save method of a ImgageModelForm

--- images/forms.py
+++ images/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 from .models import Image
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
@@ -16,3 +19,18 @@ class ImageCreateForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not match valid image extensions.')
         return url
+
+    def save(self, force_insert=False,force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        
+        response = request.urlopen(image_url)
+        image.image.save(image_name,
+        ContentFile(response.read()),
+        save=False)
+        if commit:
+            image.save()
+        return image
