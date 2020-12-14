4f70cbbaeb06005227ec09126a669e1690f5bb22
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-14 14:09:40 +0100

    add default image for users

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='pics')
+    image = models.ImageField(default='about-01.jpg', upload_to='pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
