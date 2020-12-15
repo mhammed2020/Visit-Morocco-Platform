e0aad2f062a82ca39329e61e76237ec34acfcda8
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-15 19:13:32 +0100

    add the get_absolute_url() method to Image Model

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='images_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -18,3 +19,6 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
             super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
