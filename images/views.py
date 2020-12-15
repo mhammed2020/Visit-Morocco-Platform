44b21756c217179c7e9b1b24696d4512959b1876
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-15 19:08:01 +0100

    render image object  with images detail method and using  get_object_or_404 class

--- images/views.py
+++ images/views.py
@@ -21,7 +21,8 @@ def image_create(request):
 
 
 def image_detail(request, id, slug):
-    pass
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,'images/image/detail.html', {'section': 'images', 'image': image})
 
 
 
