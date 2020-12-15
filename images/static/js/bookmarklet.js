7dafce6ab3b947251d904f01d945eb73977e5c08
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-15 15:06:32 +0100

    find images and display themby jquery

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -15,14 +15,27 @@ href: static_url + 'css/bookmarklet.css?r=' + Math.floor(Math.
 random()*99999999999999999999)
 });
 jQuery('head').append(css);
+
 // load HTML
 box_html = '<div id="bookmarklet"><a href="#" id="close">&times;</a><h1>Select an image to bookmark:</h1><div class="images"></div></div>';
-jQuery('body').append(box_html);
 jQuery('body').append(box_html);
 // close event
 jQuery('#bookmarklet #close').click(function(){
 jQuery('#bookmarklet').remove();
 });
+
+
+  // find images and display them
+  jQuery.each(jQuery('img[src$="jpg"]'), function(index, image) {
+    if (jQuery(image).width() >= min_width && jQuery(image).height()
+    >= min_height)
+    {
+      image_url = jQuery(image).attr('src');
+      jQuery('#bookmarklet .images').append('<a href="#"><img src="'+
+      image_url +'" /></a>');
+    }
+  }); //
+
 
 
   };
