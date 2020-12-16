4928cd01f8a364cc88e8ef7450ab3be90453367e
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-16 10:23:26 +0100

    Add  user_model field to User class  dynamically

--- accounts/models.py
+++ accounts/models.py
@@ -33,6 +33,11 @@ class Contact(models.Model):
     user_to = models.ForeignKey('auth.User',related_name='rel_to_set',on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True,db_index=True)
     user_model = get_user_model()
+
+    user_model.add_to_class('following',models.ManyToManyField('self',through=Contact,
+                                            related_name='followers',
+                                            symmetrical=False))
+
     class Meta:
         ordering = ('-created',)
     def __str__(self):
