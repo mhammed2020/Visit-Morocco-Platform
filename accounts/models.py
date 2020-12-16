761305cee224148b78b37e47b0d7a82f24fb9875
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-16 10:15:36 +0100

    Create many-to-many relationships for acounts models

--- accounts/models.py
+++ accounts/models.py
@@ -23,6 +23,17 @@ class Destination(models.Model) :
 
     class Meta :
         ordering = ('-date_posted',)
+    
+    # ----------------------------Tracking User---------------------------
+
+class Contact(models.Model):
+    user_from = models.ForeignKey('auth.User',related_name='rel_from_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User',related_name='rel_to_set',on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True,db_index=True)
+    class Meta:
+        ordering = ('-created',)
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
 
 
         
